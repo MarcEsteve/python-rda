4da51c3ebf1d16a32940ecb6ab8ffa4c65e1160c
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-01-28 16:26:19 +0100

    Create prova.py

---
+++ prova.py
@@ -0,0 +1,8 @@
+temperaturas= [15,25,31,11]
+print(temperaturas)
+print(temperaturas[0])
+print(temperaturas[1])
+print(temperaturas[2])
+print(temperaturas[3])
+print(temperaturas[-1])
+print(temperaturas[-2])
