349009c1a5cff7fd7d7292b539105621140d6a76
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-01-30 13:05:35 +0100

    Más sobre Python

--- python_basico/prova.py
+++
@@ -1,8 +0,0 @@
-temperaturas= [15,25,31,11]
-print(temperaturas)
-print(temperaturas[0])
-print(temperaturas[1])
-print(temperaturas[2])
-print(temperaturas[3])
-print(temperaturas[-1])
-print(temperaturas[-2])
