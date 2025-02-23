6fafa76935e7e739d7414233de3f48b9a4a8cafd
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-02-23 06:10:02 +0100

    Update 01_mails_datos.py

--- 07_email/01_mails_datos.py
+++ 07_email/01_mails_datos.py
@@ -1,17 +1,6 @@
 import imapclient
-conxGmail = imapclient.IMAPClient("imap.gmail.com", ssl=True)
-conxGmail.login("marc.esteve.garcia@gmail.com","pass")
-
-
-conxGmail.select_folder("INBOX") #Habitualmente es "INBOX" el nombre de la bandeja genérica de entrada
-
-
-mail_bruto = conxGmail.fetch([4],['BODY[]', 'FLAGS']) #El primer parámetro es el número de mensaje a recuperar
-
-
 # connOutlook = imapclient.IMAPClient("imap-mail.outlook.com", ssl=True)
 # connOutlook.login("arthas_003@hotmail.com","pass")
-
 
 # YAHOO imap.mail.yahoo.com
 # HOTMAIL imap-mail.outlook.com
@@ -24,3 +13,41 @@ mail_bruto = conxGmail.fetch([4],['BODY[]', 'FLAGS']) #El primer parámetro es e
 # GMAIL imap.gmail.com
 # OUTLOOK imap-mail.outlook.com
 # OFFICE365 outlook.office365.com
+conxGmail = imapclient.IMAPClient("imap.gmail.com", ssl=True)
+conxGmail.login("marc.esteve.garcia@gmail.com","pass")
+
+
+conxGmail.select_folder("INBOX") #Habitualmente es "INBOX" el nombre de la bandeja genérica de entrada
+
+
+# Buscar mensajes cuyo asunto contenga "WordPress"
+UIDs_asunto = conxGmail.search(['SUBJECT', 'WordPress'])
+
+if UIDs_asunto:
+    # Recuperar el contenido del primer mensaje encontrado
+    mail_bruto = conxGmail.fetch([UIDs_asunto[0]], ['BODY[]', 'FLAGS'])
+    print(mail_bruto)
+else:
+    print("No se encontraron mensajes con el asunto 'WordPress'.")
+
+if UIDs_asunto:
+    try:
+        # Recuperar el contenido del primer mensaje encontrado
+        mail_bruto = conxGmail.fetch([UIDs_asunto[0]], ['BODY[]', 'FLAGS'])
+        mensaje = mail_bruto[UIDs_asunto[0]][b'BODY[]'].decode("utf-8")
+        print(mensaje)
+    except KeyError:
+        print("No se pudo encontrar el cuerpo del mensaje.")
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+else:
+    print("No se encontraron mensajes con el asunto 'WordPress'.")
+
+mensaje.find("smtp.mailfrom=")
+
+# Me devuleve mensaje.find("smtp.mailfrom=") un -1 porque no lo encuentra
+
+# Cerrar la conexión
+conxGmail.logout()
+
+
