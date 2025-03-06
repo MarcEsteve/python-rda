4d95ed2c3c847ae7bd189684f5e62f56e6e70f61
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-03-06 18:24:27 +0100

    ultimos cambios

--- 08_interfaz/03_screenshot_reconize_img.py
+++ 08_interfaz/03_screenshot_reconize_img.py
@@ -1,2 +1,12 @@
 import pyautogui
 
+# Screenshot
+pyautogui.screenshot(r'C:/Users/artha/OneDrive/Escritorio/AKKODIS/python-rda/08_interfaz/captura.png')
+
+# Pantallazo especificando region
+pyautogui.screenshot(r'C:/Users/artha/OneDrive/Escritorio/AKKODIS/python-rda/08_interfaz/captura2.png', region=(0,0,300,400))
+
+# Localiza una imagen en pantalla
+pyautogui.locateOnScreen('galeria.png')
+
+
