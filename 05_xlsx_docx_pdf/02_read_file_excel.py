5d9df02afa10ffb74b14dab4ae2dc30ce2e3d6cd
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-06-12 16:44:12 +0200

    ejemplos excel

--- 05_xlsx_docx_pdf/02_read_file_excel.py
+++ 05_xlsx_docx_pdf/02_read_file_excel.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-df_ventas = pd.read_excel('C:/Users/artha/OneDrive/Escritorio/AKKODIS/python-rda/05_ejemplos/distritos-ejemplo.xlsx')
+df_ventas = pd.read_excel('C:/Users/artha/OneDrive/Escritorio/AKKODIS/python-rda/05_xlsx_docx_pdf/distritos-ejemplo.xlsx')
+
 df_ventas.head()
